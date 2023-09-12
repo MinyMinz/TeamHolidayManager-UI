7b5bda172c9dfee90ce8be0b1120589333353963
MinyMinz <minhaz_rahman543@outlook.com>
2023-09-12 02:13:31 +0100

    Update nav.svelte

--- src/lib/components/navbar/Nav.svelte
+++ src/lib/components/navbar/Nav.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import Loginlogoutbutton from "./loginlogoutbutton.svelte";
   import Icon from "@iconify/svelte";
   import { isLoggedIn } from "$lib/components/stores/stores";
