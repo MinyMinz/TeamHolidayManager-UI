ba96dacf08ba12f97dc7c8d29c0dcf93789bb025
MinyMinz <minhaz_rahman543@outlook.com>
2023-09-12 12:05:09 +0100

    update to install vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      fallback: 'app.html'
-    })
+    adapter: adapter({}),
   },
   preprocess: vitePreprocess(),
 };
