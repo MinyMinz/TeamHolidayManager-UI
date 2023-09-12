d78bd47d52557801cea4caef3a8fa72e063b7c15
MinyMinz <minhaz_rahman543@outlook.com>
2023-09-12 11:05:44 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    prerender: {
+      default: true,
+    },
   },
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
 };
 export default config;
