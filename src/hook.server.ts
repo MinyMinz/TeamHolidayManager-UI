74756daaeb4215d35a0bd9254abb2f8b2b458dbf
MinyMinz <minhaz_rahman543@outlook.com>
2024-12-06 15:31:50 +0000

    revert back to hook.server.ts

--- src/hook.server.ts
+++ src/hook.server.ts
@@ -4,9 +4,10 @@ export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event);
 
   response.headers.set('X-Content-Type-Options', 'nosniff');
-  // response.headers.set('X-Frame-Options', 'DENY');
+  response.headers.set('X-Frame-Options', 'DENY');
   response.headers.set('Referrer-Policy', 'no-referrer');
   response.headers.set('Permissions-Policy', 'geolocation=(), microphone=()');
+  response.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';");
 
   return response;
 };
