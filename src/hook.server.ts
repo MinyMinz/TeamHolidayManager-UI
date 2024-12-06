8aa2e7f616f0f6267829e0eca27d1eddd95024c0
MinyMinz <minhaz_rahman543@outlook.com>
2024-12-06 17:02:35 +0000

    update hook.server.ts
    alternate method to handle response headers

--- src/hook.server.ts
+++ src/hook.server.ts
@@ -1,16 +1,26 @@
 import type { Handle } from '@sveltejs/kit';
 
+// export const setSecurityHeaders = (headers: Headers) => {
+//   headers.set('X-Content-Type-Options', 'nosniff');
+//   headers.set('X-Frame-Options', 'DENY');
+//   headers.set('Referrer-Policy', 'no-referrer');
+//   headers.set('Permissions-Policy', 'geolocation=(), microphone=()');
+//   headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';");
+// };
+
+const securityHeaders ={
+  'X-Content-Type-Options': 'nosniff',
+  'X-Frame-Options': 'DENY',
+  'Referrer-Policy': 'no-referrer',
+  'Permissions-Policy': 'geolocation=(), microphone=()',
+  'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';"
+}
+
 export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event);
 
-  response.headers.set('X-Content-Type-Options', 'nosniff');
-  response.headers.set('X-Frame-Options', 'DENY');
-  response.headers.set('Referrer-Policy', 'no-referrer');
-  response.headers.set('Permissions-Policy', 'geolocation=(), microphone=()');
-  response.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';");
-
-  // Log headers to verify
-  console.log(response.headers);
-
+  Object.entries(securityHeaders).forEach(
+    ([key, value]) => response.headers.set(key, value)
+  );
   return response;
 };
