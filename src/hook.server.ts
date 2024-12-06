e26ad861fb62e6f59fda39999f21354848f0b594
MinyMinz <minhaz_rahman543@outlook.com>
2024-12-06 17:36:58 +0000

    testing sec headers

--- src/hook.server.ts
+++ src/hook.server.ts
@@ -3,7 +3,7 @@ import type { Handle } from '@sveltejs/kit';
 export const setSecurityHeaders = (headers: Headers) => {
   headers.set('X-Content-Type-Options', 'nosniff');
   headers.set('X-Frame-Options', 'DENY');
-  headers.set('Referrer-Policy', 'no-referrer');
+  headers.set('Referrer-Policy', 'no-referrer-when-downgrade');
   headers.set('Permissions-Policy', 'geolocation=(), microphone=()');
   headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';");
 };
